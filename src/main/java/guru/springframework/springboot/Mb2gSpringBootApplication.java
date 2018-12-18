898a070db02f356ea76ce9d22647e27ecccdc26a
John Thompson <john@springframework.guru>
2018-12-17 20:01:10 -0500

    initial project add

---
+++ src/main/java/guru/springframework/springboot/Mb2gSpringBootApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mb2gSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mb2gSpringBootApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/guru/springframework/springboot/Mb2gSpringBootApplicationTests.java
@@ -0,0 +1,17 @@
+package guru.springframework.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mb2gSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
